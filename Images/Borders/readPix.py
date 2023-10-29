3948c82516529dce2c4e3ee63b94606065de2a1a
Maxim <glin.maxim@yandex.ru>
2023-10-29 13:01:21 +0300

    init 0.12 commit

--- Images/Borders/readPix.py
+++ Images/Borders/readPix.py
@@ -1,15 +1,87 @@
 import cv2
+import os
+import numpy as np
 
-image = cv2.imread(r"evening\img.png")
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-dictOfPixels = dict()
-for strings in gray_image:
-    for el in strings:
-        if el in dictOfPixels:
-            dictOfPixels[el] += 1
+def createRange(x: list) -> list[list]:
+    listOfRanges = list()
+    for el in x:
+        le = len(listOfRanges)
+        for i in range(0, le):
+            if listOfRanges[i][0] < el < listOfRanges[i][1]:
+                break
+            elif np.subtract(listOfRanges[i][0], el, dtype=np.dtype(float)) < 2 and el < listOfRanges[i][0]:
+                listOfRanges[i][0] = el
+                break
+
+            elif np.subtract(el, listOfRanges[i][1], dtype=np.dtype(float)) < 2 and el > listOfRanges[i][0]:
+                listOfRanges[i][1] = el
+                break
         else:
-            dictOfPixels[el] = 1
+            listOfRanges.append([el, el])
+
+    i = 0
+    le = len(listOfRanges)
+    while True:
+        j = i + 1
+        while j < le:
+            if abs(np.subtract(listOfRanges[i][0], listOfRanges[j][1], dtype=np.dtype(float))) < 2:
+                if listOfRanges[i][0] > listOfRanges[j][0]:
+                    listOfRanges[i][0] = listOfRanges[j][0]
+                else:
+                    listOfRanges[i][1] = listOfRanges[j][1]
+
+            elif abs(np.subtract(listOfRanges[i][1], listOfRanges[j][0], dtype=np.dtype(float))) < 2:
+                if listOfRanges[i][1] < listOfRanges[j][1]:
+                    listOfRanges[i][1] = listOfRanges[j][1]
+                else:
+                    listOfRanges[i][0] = listOfRanges[j][0]
+
+            else:
+                j += 1
+                continue
+
+            listOfRanges.pop(j)
+            le -= 1
+
+        else:
+            i += 1
+
+        if i > le - 1:
+            break
+
+    i = 0
+    while i < le:
+        if np.subtract(listOfRanges[i][1], listOfRanges[i][0], dtype=np.dtype(float)) < 3:
+            listOfRanges.pop(i)
+            le -= 1
+        else:
+            i += 1
+
+    return listOfRanges
 
 
-print(list(reversed(sorted(dictOfPixels.items(), key=lambda x: x[1]))))
+folders = os.listdir(path=".")
+
+for folder in folders:
+    if folder == "readPix.py":
+        continue
+
+    files = os.listdir(path=f"./{folder}")
+    dictOfPixels = dict()
+
+    for file in files:
+        image = cv2.imread(rf"evening\{file}")
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        for strings in gray_image:
+            for el in strings:
+                if el in dictOfPixels:
+                    dictOfPixels[el] += 1
+                else:
+                    dictOfPixels[el] = 1
+
+    ranges = createRange(list(dictOfPixels.keys()))
+    print(f"{folder} range is", end=" ")
+    print(*ranges)
+
