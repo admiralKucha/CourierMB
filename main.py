e8e14f2cb439d1c51f1a66a3fa047948bc71e644
Maxim <glin.maxim@yandex.ru>
2023-10-31 22:48:22 +0300

    init 0.32 commit fog and morning may have errors fog range have little mistake normal needs more information(also very similar to morning) (Кудан и Курав)
    now we can check time of day, so fog and evening filter work better!
    
    fog works not normal
    think about filter water and grass in black
    necessary to initialize the time position and enable the filter depending on the time

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ import os
 import numpy as np
 
 
+def timeOfDay(image: np.ndarray):
+    y, h = 1054, 19
+    x, w = 1625, 206
+    crop_img = image[y:y + h, x:x + w]
+    gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+    for i in range(1, 4):
+        img1 = cv2.imread(rf"Images\time\img{i}.png")
+        img1 = img1.T[0].T
+
+        if np.array_equal(img1, gray):
+            return True
+
+    return False
+
 def filterImg(image: np.ndarray, range: list) -> np.ndarray:
     global black
 
@@ -11,16 +25,16 @@ def filterImg(image: np.ndarray, range: list) -> np.ndarray:
     result = cv2.bitwise_and(gray, gray, mask=mask)
     result[mask > 0] = 255
 
-
     if (shapeIm[0] * shapeIm[1]) / 10 < cv2.countNonZero(result):
         result = black.copy()
 
     return result
 
-#[79, 97]
-listOfRanges = [[16, 36], [79, 97], [97, 122], [27, 76], [40, 70]]
 
-folderTest = "normal"
+listOfNight = [[16, 36]]
+listOfRanges = [[79, 97], [97, 122], [27, 76], [40, 70]]
+
+folderTest = "morning"
 path = rf"Images\Screenshoots\{folderTest}"
 pathToWrite = rf"Images\results\{folderTest}"
 
@@ -29,13 +43,20 @@ files = os.listdir(path=fr".\{path}")
 
 for file in files:
     image = cv2.imread(rf"{path}\{file}")
+    flag = timeOfDay(image)
     shapeIm = image.shape[0:2]
     black = np.zeros(shapeIm, dtype=np.uint8)
     allRangesImg = black.copy()
 
-    for range in listOfRanges:
-        buf = filterImg(image, range)
-        allRangesImg = cv2.bitwise_or(allRangesImg, buf)
+    if flag:
+        for rang in listOfNight:
+            buf = filterImg(image, rang)
+            allRangesImg = cv2.bitwise_or(allRangesImg, buf)
+
+    else:
+        for rang in listOfRanges:
+            buf = filterImg(image, rang)
+            allRangesImg = cv2.bitwise_or(allRangesImg, buf)
 
 
     contours, _ = cv2.findContours(allRangesImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -49,6 +70,7 @@ for file in files:
         # Проверка длины и ширины контура
         if w>30 and h>30:
             realContours.append([x, x+w, y, y+h])
+        #cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 
     i = 0
